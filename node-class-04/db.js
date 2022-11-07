9da539f88ef029cc25f00e28cc8b7143e3e0b2d0
gabrielforster <rochafrgabriel@gmail.com>
2022-11-07 08:02:36 -0300

    feat / route

--- node-class-04/db.js
+++ node-class-04/db.js
@@ -3,7 +3,7 @@ import mysql from "mysql2"
 const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Rocha1307###$",
+    password: "root",
     database: "node_04"
 })
 
--- node-class-04/index.js
+++ node-class-04/index.js
@@ -10,7 +10,7 @@ const app = express()
 app.use(express.static(__dirname + "/public"))
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get("/", (req, res) => { res.send("Hello World!")} )
+app.get("/", (req, res) => res.sendFile(__dirname + "/public/index.html"))
 
 app.get("/cadastro", (req, res) => res.sendFile(__dirname + "/public/cadastro.html"))
 
