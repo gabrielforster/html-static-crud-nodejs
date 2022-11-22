aac881cfeda1b719b4c6f951a579ab9b440a77bc
Gabriel Rocha <rochafrgabriel@gmail.com>
2022-11-22 10:13:05 -0300

    feat: create the edit modal on frontend

--- pizzaria-fullstack-ornot/app/produtos/main.js
+++ pizzaria-fullstack-ornot/app/produtos/main.js
@@ -1,10 +1,49 @@
 const app = document.getElementById("app")
+let selected;
 
 async function fetchProdutos(){
   const response = await fetch('/api/produto')
   return await response.json()
 }
 
+function setSelected(productId){
+  selected = productId
+}
+
+async function fillForm(){
+  const response = await fetch(`/api/produto/${selected}`)
+  const produto = await response.json()
+
+  const nomeInput = document.getElementById("nome")
+  const quantidadeInput = document.getElementById("quantidade")
+  const valorInput = document.getElementById("valor")
+  const idInput = document.getElementById("id")
+
+  nomeInput.value = produto.nome
+  quantidadeInput.value = produto.quantidade
+  valorInput.value = produto.valor
+  idInput.value = produto.id
+}
+
+function createModalListeners(){
+  const modals = document.querySelectorAll('[data-modal]');
+
+  modals.forEach(trigger => {
+    trigger.addEventListener('click', async event => {
+      event.preventDefault();
+      await fillForm();
+      const modal = document.getElementById(trigger.dataset.modal);
+      modal.classList.add('open');
+      const exits = modal.querySelectorAll('.modal-exit');
+      exits.forEach(exit => {
+        exit.addEventListener('click', event => {
+          event.preventDefault();
+          modal.classList.remove('open');
+        });
+      });
+    });
+  });  
+}
 
 async function mountTable(){
   const produtos = await fetchProdutos()
@@ -17,6 +56,7 @@ async function mountTable(){
         <th>Tipo</th>
         <th>Quantidade</th>
         <th>Valor</th>
+        <th>Ações</th>
       </tr>
     </thead>
     <tbody>
@@ -26,12 +66,22 @@ async function mountTable(){
           <td>${produto.tipo}</td>
           <td>${produto.quantidade}</td>
           <td>${produto.valor}</td>
+          <td>
+            <a
+              onclick="setSelected(${produto.id})"
+              data-modal="edit-modal"
+            >
+              Editar
+            </a>
+            <a>Excluir</a>
+          </td>
         </tr>
       `).join("")}
     </tbody>
   `
 
   app.appendChild(table)
+  createModalListeners()
 }
 
 mountTable()
